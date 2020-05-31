a8dc1a3704565d5bcac77e467179d6d12367f812
John Baker <berehhahn@gmail.com>
2020-05-31 14:25:25 -0400

    After a bunch of work.  Now ready to try.

--- CovidModel.py
+++ CovidModel.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
+
+verbose=False
+def vprint(*args):
+    if verbose: print(*args)
+
 def model(ts,pars):
   '''  
     Model:
@@ -40,9 +50,9 @@ def model_err(ts, pars, sigma, pm):
   q = q0 + ( pm*sigma - q0*(E-E0) ) / ( E -1 )
   #try to assure that f+/f>1 and f'+/f'>1 :dq=q0*|dd|*t0 ??
   q = q0*(E0/E) 
-  print('dq=',q-q0)
+  vprint('dq=',q-q0)
   z = z0 + pm*sigma - (q-q0)
-  print('dz',z-z0)
+  vprint('dz',z-z0)
   result = z - q * np.exp(-dt*d)
   return result
 
@@ -108,13 +118,13 @@ def model_llsf(ts,ys,ws,pars0,stats=True):
 
   #normalize
   ws=ws/sum(ws)
-  #print('ws',ws)
+  #vprint('ws',ws)
 
   #set t0 as mean of ts
   t0 = sum(ws*ts)
   dt = ts-t0
   Es = np.exp(-dt*d0)
-  #print('t0,dt,Es:',t0,dt,Es)
+  #vprint('t0,dt,Es:',t0,dt,Es)
 
   #compute basic means
   wE    = sum(ws*Es)
@@ -125,7 +135,7 @@ def model_llsf(ts,ys,ws,pars0,stats=True):
   wY    = sum(ws*ys)
   wYE   = sum(ws*ys*Es)
   wYEt  = sum(ws*ys*Es*dt)
-  #print('wE,wE2',wE,wE2)
+  #vprint('wE,wE2',wE,wE2)
 
   #secondary quantities
   wExE   = wE2  - wE*wE
@@ -133,14 +143,14 @@ def model_llsf(ts,ys,ws,pars0,stats=True):
   wEtxEt = wE2t2 - wEt*wEt
   wYxE   = wYE  - wY*wE
   wYxEt  = wYEt - wY*wEt
-  #print('wExE:', wExE)
+  #vprint('wExE:', wExE)
 
   #solve
   q0dd =  ( wYxE*wEtxE - wYxEt*wExE ) /  ( wEtxE*wEtxE - wEtxEt*wExE )
   dnew = d0 + q0dd/q0 
   qnew = (- wYxE  + q0dd*wEtxE ) / wExE
   znew = wY + qnew*wE - q0dd*wEt
-  #print('q0,d0,q0dd',q0,d0,q0dd)
+  #vprint('q0,d0,q0dd',q0,d0,q0dd)
 
   pars={}
   pars['z']  = znew
@@ -157,7 +167,7 @@ def model_llsf(ts,ys,ws,pars0,stats=True):
       ynew = model(ts,pars0)
       residual = ys - ynew  
       F = sum(ws*residual*residual)
-      print('sigma,sigma0:',pars['sigma'],np.sqrt(F))
+      vprint('sigma,sigma0:',pars['sigma'],np.sqrt(F))
      
   return pars
 
@@ -171,26 +181,120 @@ def model_lsf(ts,ys,ws,stats=True):
     dd=pars['d']-pars0['d']
     dp = (dq)**2/(pars['q']+pars0['q'])**2 + (dd)**2/(pars['d']+pars0['d'])**2
     pars0=dict(pars)
-    print('dp,pars:',dp,pars)
+    vprint('dp,pars:',dp,pars)
     pars['d']-=dd*0.1
     pars['q']-=dq*0.1
   return pars
 
-def make_model(ts, ys, n, tend, wtpow=0,it=None):
+def make_model(ts, ys, n, tend, wtpow=0,it=None,nsigma=2):
   #make model from partial series up to entry it using data of length n
   if it is not None:
     iend=it+1
   else:
     iend=len(ts)
   istart=max([0,iend-n])
-  print('Fitting from ',ts[istart],'to',ts[iend-1])
+  vprint('Fitting from ',ts[istart],'to',ts[iend-1])
   ws=np.exp(wtpow*ys)
-  #print('ws:',ws)
+  #vprint('ws:',ws)
   pars=model_lsf(ts[istart:iend],ys[istart:iend],ws[istart:iend],stats=True)
   dt=1
   t=np.arange(ts[0],tend)
   f=model(t,pars)
-  fplus=model_err(t,pars,pars['sigma'],1)
-  fminus=model_err(t,pars,pars['sigma'],-1)
+  fplus=model_err(t,pars,pars['sigma'],1*nsigma)
+  fminus=model_err(t,pars,pars['sigma'],-1*nsigma)
   return [t,f,fplus,fminus]
 
+def show_model(datafile='MoCoCovidData.csv',fitdays=None,fitwidth=30,nextrap=45,minday=10,delta=False):
+    coviddata=pd.read_csv(datafile)
+    ts=coviddata.index.values+1
+    ys=np.log(coviddata['Moco cases'].dropna().values)
+    ndata=len(ys)
+    ts=ts[:ndata]
+    if fitdays is None:
+        #print('computing fitdays. last day is',ts[-1])
+        maxfits=6
+        spacing=7
+        fitdays=ts[(ts-2)%spacing==0]
+        #print('fitdays=',fitdays)
+        if len(fitdays)>maxfits: fitdays=fitdays[-maxfits:]
+        #print('fitdays=',fitdays)
+  
+    #create fig
+    colors=['b','g','r','c','m','y']
+    fig,axs=plt.subplots(2,figsize=(15,20))
+    ax0=axs[0]
+    ax1=axs[1]
+    #locator = mdates.AutoDateLocator(minticks=int(ndata/14), maxticks=20)
+    locator=mdates.MonthLocator()
+    minorloc=mdates.WeekdayLocator(mdates.FR)
+    #formatter = mdates.ConciseDateFormatter(locator)
+    formatter = mdates.DateFormatter('%b')
+    minorform = mdates.DateFormatter('%m-%d')
+    ax0.xaxis.set_major_locator(locator)
+    ax0.xaxis.set_minor_locator(minorloc)    
+    ax0.xaxis.set_major_formatter(formatter)
+    ax0.xaxis.set_minor_formatter(minorform)
+    ax0.xaxis.set_tick_params(which='major',pad=15)
+    ax1.xaxis.set_major_locator(locator)
+    ax1.xaxis.set_minor_locator(minorloc)    
+    ax1.xaxis.set_major_formatter(formatter)
+    ax1.xaxis.set_minor_formatter(minorform)
+    ax1.xaxis.set_tick_params(which='major',pad=15)
+    ax0.grid(axis='x',which='minor')
+    ax0.grid(axis='y',which='major')
+    ax1.grid(axis='x',which='minor')
+    ax1.grid(axis='y',which='major')
+    
+    datebase = np.datetime64('2020-03-04')
+    ic=0
+    for fitday in fitdays:
+        #prep for fit
+        if fitday is None: fitday=ts[-1]
+        tmax=ts[-1]+nextrap
+        tmin=10
+        ifit=len(ts[ts<=fitday+0.5])
+        imin=len(ts[ts<minday])
+        fw=fitwidth
+        if ifit-fw<imin: fw=ifit-imin
+        itmin=len(ts[ts<=tmin])
+        fts=ts[itmin:ifit]
+        fys=ys[itmin:ifit]
+        #print('fitting over {} <= t <= {}'.format(fts[0],fts[-1]))
+        
+        c=colors[len(colors)-1-ic]
+        ic+=1
+        fade=0.6**(len(fitdays)-ic)
+        #make_model (x weight)
+        [t,f,fm,fp]=make_model(fts,fys,fw,tmax,1)
+        #print('x weight',f3m,f3p)
+        if delta: #plot day-to-day difference
+            ax0.plot(datebase+t[1:],f[1:]-f[:-1],c,label='x weight',alpha=fade)
+            ax1.plot(datebase+t[1:],np.exp(f[1:])-np.exp(f[:-1]),c,label='x weight',alpha=fade)
+            ax0.plot(datebase+fts[-1:],fys[-1:]-fys[-2:-1],c+'.',ms=20)
+            ax1.plot(datebase+fts[-1:],np.exp(fys[-1:])-np.exp(fys[-2:-1]),c+'.',ms=20)
+            ax0.fill_between(datebase+t[1:],
+                             fm[1:]-fm[:-1],
+                             fp[1:]-fp[:-1],
+                             color=c,alpha=0.2*fade)
+            ax1.fill_between(datebase+t[1:],
+                             np.exp(fm[1:])-np.exp(fm[:-1]),
+                             np.exp(fp[1:])-np.exp(fp[:-1]),
+                             color=c,alpha=0.2*fade)
+        else:
+            ax0.plot(datebase+t,[max([0,x])for x in f],c,label='x weight',alpha=fade)
+            ax1.plot(datebase+t[0:],np.exp(f),c,label='x weight',alpha=fade)
+            ax0.plot(datebase+fts[-1:],fys[-1:],c+'.',ms=20)
+            ax1.plot(datebase+fts[-1:],np.exp(fys[-1:]),c+'.',ms=20)
+            ax0.fill_between(datebase+t,[max([0,x])for x in fm],[max([0,x])for x in fp],color=c,alpha=0.2*fade)
+            ax1.fill_between(datebase+t,np.exp(fm),np.exp(fp),color=c,alpha=0.2*fade)
+
+    #plot data
+    c='k'
+    if delta:
+        ax0.plot(datebase+ts[1:],ys[1:]-ys[:-1],c+'.',ms=10)
+        ax1.plot(datebase+ts[1:],np.exp(ys[1:])-np.exp(ys[:-1]),c+'.',ms=10)
+    else:
+        ax0.plot(datebase+ts,ys,c+'.',ms=10)
+        ax1.plot(datebase+ts,np.exp(ys),c+'.',ms=10)
+    
+    #return fig
