27b55d660f54d6e61253ecaa8dbc399b3529c79f
JohnGBaker <john.g.baker@nasa.gov>
2020-06-26 16:30:07 -0400

    Update CovidModel.py

--- CovidModel.py
+++ CovidModel.py
@@ -149,7 +149,7 @@ def model_llsf(ts,ys,ws,pars0,stats=True):
   #solve
   q0dd =  ( wYxE*wEtxE - wYxEt*wExE ) /  ( wEtxE*wEtxE - wEtxEt*wExE )
   dnew = d0 + q0dd/q0 
-  qnew = (- wYxE  + q0dd*wEtxE ) / wExE
+  qnew = min([0,(- wYxE  + q0dd*wEtxE ) / wExE])
   znew = wY + qnew*wE - q0dd*wEt
   #vprint('q0,d0,q0dd',q0,d0,q0dd)
 
