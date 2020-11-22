e70cd35b4b73c81ad93e4c06bd4a52499d0240ce
JohnGBaker <john.g.baker@nasa.gov>
2020-11-22 12:10:36 -0500

    Update CovidModel.py

--- CovidModel.py
+++ CovidModel.py
@@ -327,5 +327,6 @@ def show_model(datafile='MoCoCovidData.csv',fitdays=None,fitwidth=30,nextrap=45,
         ax1.plot(datebase+ts,np.exp(ys),c+'.',ms=10)
     ymax*=1.5 
     if plt.ylim()[1]>ymax: plt.ylim(top=ymax)
+    plt.ylim(bottom=-0.02*ymax)
     print('ylim=',plt.ylim(),'ymax=',ymax)
     #return fig
