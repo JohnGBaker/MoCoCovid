49a2280906c1ba7d15281b72730d19011d940af1
JohnGBaker <john.g.baker@nasa.gov>
2021-09-10 14:47:09 -0400

    Update CovidModel.py

--- CovidModel.py
+++ CovidModel.py
@@ -226,7 +226,7 @@ def make_model(ts, ys, n, tend, wtpow=0,it=None,nsigma=2):
   fminus=model_err(t,pars,pars['sigma'],-1*nsigma)
   return [t,f,fplus,fminus]
 
-def show_model(datafile='MoCoCovidData.csv',fitdays=None,fitwidth=30,nextrap=45,minday=10,delta=False,col="Moco",ylimfac=1.5,loft=False):
+def show_model(datafile='MoCoCovidData.csv',fitdays=None,fitwidth=30,nextrap=45,minday=10,delta=False,col="Moco",ylimfac=1.5,logy=False):
     coviddata=pd.read_csv(datafile)
     ts=coviddata.index.values+1
     ys=np.log(coviddata[col+' cases'].dropna().values)
